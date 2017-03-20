330d593b465f76f6f38a1654f93b2f64495eb6c0
Alexis Benoist <alexis.benoist@gmail.com>
2017-03-20 13:24:45 +0100

    session counter

--- test.js
+++ test.js
@@ -20,9 +20,34 @@ var http = require('http')
 //   }
 // })
 var app = express()
+var cookieParser = require('cookie-parser')
+app.use(cookieParser())
+var sessOpt = {
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 6000 }
+};
+app.use(session(sessOpt))
+
 
 app.get('/', function(req, res, next) {
   res.send('Hello world!')
+})
+
+app.get('/counter', function(req, res, next) {
+  var sess = req.session;
+  console.log(sess)
+  if (sess.views) {
+    sess.views++
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<p>views: ' + sess.views + '</p>')
+    res.write('<p>expires in: ' + (sess.cookie.maxAge / 1000) + 's</p>')
+    res.end()
+  } else {
+    sess.views = 1
+    res.end('welcome to the session demo. refresh!')
+  }
 })
 
 const server = new http.Server(app);
